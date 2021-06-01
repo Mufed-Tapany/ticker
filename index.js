de2100cc1adb185cdca86f32105c5bcfaaa5d58a
Mufed Tapany <mufedtapany@gmail.com>
2021-06-01 21:28:40 +0200

    Ticker Project - enhancing code

--- index.js
+++ index.js
@@ -1,86 +1,88 @@
 (function () {
-    var newsContainer = document.getElementById("news");
-    var newsContainerBottom = document.getElementById("newsBottom");
-    var newsLinksTop = newsContainer.getElementsByTagName("A");
-    var newsLinksBottom = newsContainerBottom.getElementsByTagName("A");
-    var left = newsContainer.offsetLeft;
-    var right =
-        window.innerWidth -
-        newsContainerBottom.offsetLeft -
-        newsContainerBottom.offsetWidth; // getting the offsetRight of the bottom container
-    var animationId;
-    var animationIdBottom;
-    var isMovingTop = true;
-    var isMovingBottom = true;
+    initTicker("news", "newsBottom");
 
-    hoverEffect();
-    moveNewsTickerTop();
-    moveNewsTickerBottom();
+    function initTicker(top, bottom) {
+        var newsContainer = document.getElementById(top);
+        var newsContainerBottom = document.getElementById(bottom);
+        var newsLinksTop = newsContainer.getElementsByTagName("A");
+        var newsLinksBottom = newsContainerBottom.getElementsByTagName("A");
+        var left = newsContainer.offsetLeft;
+        var right =
+            window.innerWidth -
+            newsContainerBottom.offsetLeft -
+            newsContainerBottom.offsetWidth; // getting the offsetRight of the bottom container
+        var animationId;
+        var animationIdBottom;
+        var isMovingTop = true;
+        var isMovingBottom = true;
+        function moveNewsTickerTop() {
+            left--;
 
-    function moveNewsTickerTop() {
-        left--;
+            if (left <= -newsLinksTop[0].offsetWidth) {
+                left += newsLinksTop[0].offsetWidth;
+                newsContainer.appendChild(newsLinksTop[0]);
+            }
 
-        if (left <= -newsLinksTop[0].offsetWidth) {
-            left += newsLinksTop[0].offsetWidth;
-            newsContainer.appendChild(newsLinksTop[0]);
+            newsContainer.style.left = left + "px";
+
+            animationId = requestAnimationFrame(moveNewsTickerTop);
         }
 
-        newsContainer.style.left = left + "px";
+        function moveNewsTickerBottom() {
+            right--;
 
-        animationId = requestAnimationFrame(moveNewsTickerTop);
-    }
+            var lastNewsLink = newsContainerBottom.lastElementChild;
 
-    function moveNewsTickerBottom() {
-        right--;
+            if (right <= lastNewsLink.offsetWidth) {
+                right += lastNewsLink.offsetWidth;
+                newsContainerBottom.insertBefore(
+                    lastNewsLink,
+                    newsContainerBottom.firstChild
+                );
+            }
+            newsContainerBottom.style.right = right + "px";
 
-        var lastNewsLink = newsContainerBottom.lastElementChild;
-
-        if (right <= lastNewsLink.offsetWidth) {
-            right += lastNewsLink.offsetWidth;
-            newsContainerBottom.insertBefore(
-                lastNewsLink,
-                newsContainerBottom.firstChild
-            );
+            animationIdBottom = requestAnimationFrame(moveNewsTickerBottom);
         }
-        newsContainerBottom.style.right = right + "px";
 
-        animationIdBottom = requestAnimationFrame(moveNewsTickerBottom);
-    }
+        function hoverEffect() {
+            for (var i of newsLinksTop) {
+                var link = i;
+                isMovingBottom = true;
+                link.addEventListener("mousemove", function (event) {
+                    isMovingTop = false;
+                    this.style.color = "blue";
+                    this.style.textDecoration = "underline";
+                    cancelAnimationFrame(animationId);
+                });
+                link.addEventListener("mouseleave", function (event) {
+                    isMovingTop = true;
+                    this.style.color = "";
+                    this.style.textDecoration = "";
+                    moveNewsTickerTop();
+                    event.stopPropagation();
+                });
+            }
+        }
 
-    function hoverEffect() {
-        for (var i of newsLinksTop) {
+        for (var i of newsLinksBottom) {
             var link = i;
-            isMovingBottom = true;
             link.addEventListener("mousemove", function (event) {
-                isMovingTop = false;
+                isMovingBottom = false;
                 this.style.color = "blue";
                 this.style.textDecoration = "underline";
-                cancelAnimationFrame(animationId);
+                cancelAnimationFrame(animationIdBottom);
             });
             link.addEventListener("mouseleave", function (event) {
-                isMovingTop = true;
+                isMovingBottom = true;
                 this.style.color = "";
                 this.style.textDecoration = "";
-                moveNewsTickerTop();
-                this.stopPropagation();
+                moveNewsTickerBottom();
+                event.stopPropagation();
             });
         }
-    }
-
-    for (var i of newsLinksBottom) {
-        var link = i;
-        link.addEventListener("mousemove", function (event) {
-            isMovingBottom = false;
-            this.style.color = "blue";
-            this.style.textDecoration = "underline";
-            cancelAnimationFrame(animationIdBottom);
-        });
-        link.addEventListener("mouseleave", function (event) {
-            isMovingBottom = true;
-            this.style.color = "";
-            this.style.textDecoration = "";
-            moveNewsTickerBottom();
-            this.stopPropagation();
-        });
+        hoverEffect();
+        moveNewsTickerTop();
+        moveNewsTickerBottom();
     }
 })();
